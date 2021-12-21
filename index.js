fa24b7c068bc76c3753e5a66791533f75a5e94ef
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2021-12-21 15:02:17 -0300

    update 6

--- index.js
+++ index.js
@@ -5,21 +5,34 @@ const fs = require('fs');
 let rawdata = fs.readFileSync('produtos.json');
 let products = JSON.parse(rawdata);
 
+var bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
 const pageFolder = __dirname.replace("vacsinaserver", "vacsina");
-
 app.use(express.static(pageFolder));
-app.get('*', function(req, res) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('*', function(req, res){
+  var conteudo = JSON.stringify(req.body);
+  var fs = require('fs');
+  fs.writeFile('nome-do-ficheiro.txt', conteudo, 'utf8', function (err) {
+    if (err) throw err;
+    // correr código aqui depois do ficheiro estar gravado
+
+  });
+});
+
+/*app.get('*', function(req, res) {
   // `req` is an instance of Node.js' built-in HTTP request class,
   // with some additional features from Express
   req instanceof require('http').IncomingMessage; // true
   //res.send('id: ' + req.query.id);
   //console.log(req.params.id);
-  const arry = req.query.id != undefined ? products.filter(x => {return x.id == req.query.id}) : products;
+  const arry = req.query["input-mapname"] != undefined ? products.filter(x => {return x.id == req.query.id}) : products;
   res.json(arry);
-});
+});*/
 
 app.listen(8000);
 
