9454ba5d5869d0ac76c9f4a78dcd0938ed5448a5
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2021-12-21 12:48:25 -0300

    update 1

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('produtos.json');
+let products = JSON.parse(rawdata);
+console.log(products);
+
+const express = require('express');
+const app = express();
+
+app.get('*', function(req, res) {
+  // `req` is an instance of Node.js' built-in HTTP request class,
+  // with some additional features from Express
+  req instanceof require('http').IncomingMessage; // true
+  //res.send('id: ' + req.query.id);
+  //console.log(req.params.id);
+  const arry = req.query.id != undefined ? products.filter(x => {return x.id == req.query.id}) : products;
+  res.json(arry);
+});
+app.listen(3000);
+
+var static = require('node-static');
+var http = require('http');
+
+var file = new(static.Server)(__dirname);
+
+http.createServer(function (req, res) {
+  file.serve(req, res);
+}).listen(8000);
