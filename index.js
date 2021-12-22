34d43d56af8de80908347313758af4872baf34c0
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2021-12-22 09:30:39 -0300

    a

--- index.js
+++ index.js
@@ -18,29 +18,7 @@ app.post('/editor/newmap', function(req, res){
   var conteudo = JSON.stringify(req.body);
   fs.writeFile('nome-do-ficheiro.txt', conteudo, 'utf8', function (err) {
     if (err) throw err;
-    // correr código aqui depois do ficheiro estar gravado
-
   });
   res.redirect('/editor');
 });
-
-/*app.get('*', function(req, res) {
-  // `req` is an instance of Node.js' built-in HTTP request class,
-  // with some additional features from Express
-  req instanceof require('http').IncomingMessage; // true
-  //res.send('id: ' + req.query.id);
-  //console.log(req.params.id);
-  const arry = req.query["input-mapname"] != undefined ? products.filter(x => {return x.id == req.query.id}) : products;
-  res.json(arry);
-});*/
-
 app.listen(8000);
-
-/*var static = require('node-static');
-var http = require('http');
-
-var file = new(static.Server)(pageFolder);
-
-http.createServer(function (req, res) {
-  file.serve(req, res);
-}).listen(8000, "0.0.0.0");*/
